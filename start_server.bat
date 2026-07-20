37a092195c19dbfef9af7f1be5524dc712721e95
TE Digital 360 <tedigital360@te.gob.pa>
2026-07-20 07:51:32 -0500

    chore: eliminar start_server.bat redundante - usar scripts/iniciar_servidor.bat

--- start_server.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\PC\Documents\practica profesional\TE Digital 360 Modular\backend"
-set PYTHONPATH=C:\Users\PC\Documents\practica profesional\TE Digital 360 Modular\backend
-start /B python server.py
